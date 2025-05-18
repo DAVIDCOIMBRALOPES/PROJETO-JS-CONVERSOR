e7ca1adeb57615db5d8c3466e973a38ecabe5c7d
DAVIDCOIMBRALOPES <davidcoimbralopess@gmail.com>
2025-05-18 18:36:26 -0300

    Conversor de moda finalizado

--- projetoconversor.js
+++ projetoconversor.js
@@ -1,6 +1,115 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-console.log(convertButton)
 
+
+
+function convertValues(){
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector (".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector (".currency-value")
+    
+    
+
+    console.log(currencySelect.value)
+
+
+    const dolarToday = 6
+
+    const euroToday = 7
+
+    const libraToday= 8
+
+    const realToday = 1
+
+
+
+
+    if(currencySelect.value  == "dolar"){
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+
+        style: "currency",
+        currency: "USD"
+
+
+    } ).format(inputCurrencyValue / dolarToday)
+
+
+    }
+
+    if(currencySelect.value  == "euro"){
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+
+            style: "currency",
+            currency: "EUR"
+    
+    
+        } ).format(inputCurrencyValue / euroToday)
+
+        
+    }
+
+    if(currencySelect.value  == "libra"){
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+
+            style: "currency",
+            currency: "GBP"
+    
+    
+        } ).format(inputCurrencyValue / libraToday)
+
+        
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { 
+
+        style: "currency",
+        currency: "BRL"
+
+    } ).format(inputCurrencyValue)
+
+    
+
+
+  
+}
+
+function  changeCurrency (){
+
+    const currencyName = document.getElementById("currency-dolar")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if(currencySelect.value  == "dolar"){
+
+    currencyName.innerHTML = "Dolar Americano"
+    currencyImage.src = "./assets/estados-unidos (1) 1.png"
+    
+    }
+
+
+    if(currencySelect.value  == "euro"){
+
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/Euro.png"
+}
+
+
+        if(currencySelect.value  == "libra"){
+
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/Libra.png"
+}
+
+convertValues()
+
+
+}
+currencySelect.addEventListener ("change" , changeCurrency)
+convertButton.addEventListener("click", convertValues)
 
 
